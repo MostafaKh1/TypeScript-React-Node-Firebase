143578c21d4236a762b28f0a2f7b88d27ff5ba05
MostafaKh1 <chiiit1754@gmail.com>
2023-10-12 07:22:51 +0300

    .env

--- server/src/firebaseConfig.ts
+++ server/src/firebaseConfig.ts
@@ -1,14 +1,15 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-
+import dotenv from "dotenv";
+dotenv.config();
 const firebaseConfig = {
-  apiKey: "AIzaSyCOS9wXt1LNDDO1VNr9PE3c70iwZp_dxZw",
-  authDomain: "node-firebase-a0cd0.firebaseapp.com",
-  projectId: "node-firebase-a0cd0",
-  storageBucket: "node-firebase-a0cd0.appspot.com",
-  messagingSenderId: "357643154115",
-  appId: "1:357643154115:web:c64240eb3e938a68cba876",
-  measurementId: "G-SMJ5TEK3XV",
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
